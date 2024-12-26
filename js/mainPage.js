7e9aca7a5e7bb858a024d5a633765585ac7ab802
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-12-26 20:21:09 +0500

    New Commit

--- js/mainPage.js
+++ js/mainPage.js
@@ -6,7 +6,12 @@ const HTTP = "http://localhost:3000/students"
 let elModalWrapper = document.querySelector(".modal_wrapper")
 let elModalInner = document.querySelector(".modal_inner")
 let elStudentsList = document.querySelector(".students_list")
+let elSearch = document.querySelector(".search_input")
 
+
+
+let date = new Date()
+const signedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
 
 // Add Function start
 elModalWrapper.addEventListener("click", (e)=> {
@@ -34,14 +39,12 @@ function handleAddBtn(){
                 <input required type="tel" name="phone" placeholder="Phone" class="w-full border h-[50px] rounded-md text-[20px] p-2 border-[2px] outline-none focus:border-yellow-500">
                 <label class="text-[20px] text-slate-500 font-bold">Enroll Number</label>
                 <input required type="number" name="enrollNumber" placeholder="Enroll Number" class="w-full border h-[50px] rounded-md text-[20px] p-2 border-[2px] outline-none focus:border-yellow-500">
-                <label class="text-[20px] text-slate-500 font-bold">Date admission</label>
-                <input type="date" name="dateAdmisson" placeholder="Date admission" class="w-full border h-[50px] rounded-md text-[20px] p-2 border-[2px] outline-none focus:border-yellow-500">
               </div>
               <button type="submit" class="addBtn w-full bg-yellow-500 h-[50px] rounded-xl font-bold text-white text-[20px]">Add</button>
            </form>
     `
 
-    let elInputFile = document.querySelector(".dateAdmisson")
+    let elInputFile = document.querySelector(".input_file")
     let elImage = document.querySelector(".image")
 
     elInputFile.addEventListener("change", (e)=> {
@@ -49,7 +52,7 @@ function handleAddBtn(){
     })
 
     const elStudentsForm = document.querySelector(".addStudents")
-
+    
     elStudentsForm.addEventListener("submit", (e)=> {
         e.preventDefault()
         const data = {
@@ -57,10 +60,9 @@ function handleAddBtn(){
             email:e.target.email.value,
             phone:e.target.phone.value,
             enrollNumber:e.target.enrollNumber.value,
-            dateAdmisson:e.target.dateAdmisson.value,
+            dateAdmisson:signedDate,
             image:elImage.src
         }
-
         axios.post(HTTP, data).then(res => {
             alert("Success")
             e.target.reset()
@@ -70,10 +72,32 @@ function handleAddBtn(){
 // Add Function end
 
 // Get users start
-function renderUsers(list){
-    axios.get(HTTP)
-    .then(res => {
-        res.data.map((item) => {
+function renderUsers(list,data){
+    if(!data) {
+        axios.get(HTTP).then(res => {
+            res.data.map((item) => {
+                let elTr = document.createElement("tr")
+                elTr.className = "bg-slate-100 w-full p-5 rounded-md"
+                elTr.innerHTML = `
+                    <td class="py-[7px]"><img src="${item.image}" alt="altImage" width="65" height="55" class="rounded-md"></td>
+                               <td class="py-[7px] text-[15px] leading-[17px]"><strong>${item.username}</strong></td>
+                               <td class="py-[7px] text-[15px] leading-[17px]">${item.email}</td>
+                               <td class="py-[7px] text-[15px] leading-[17px]">${item.phone}</td>
+                               <td class="py-[7px] text-[15px] leading-[17px]">${item.enrollNumber}</td>
+                               <td class="py-[7px] text-[15px] leading-[17px]">${item.dateAdmisson}</td>
+                               <td class="py-[7px] leading-[17px]">
+                               <button class="text-[25px] text-[#FEAF00] mr-3"><i class="fa-solid fa-circle-info"></i></button>
+                               <button onclick="handleEdit('${item.id}')" class="text-[25px] text-[#FEAF00] mr-3"><i class="fa-solid fa-pen-to-square"></i></button>
+                               <button onclick="handleDelete('${item.id}')" class="text-[25px] text-[#FEAF00] mr-3"><i class="fa-solid fa-trash"></i></button>
+                    </td>
+                `
+                list.append(elTr)
+            })
+        })
+    }
+    else{
+        list.innerHTML = null
+        data.map((item) => {
             let elTr = document.createElement("tr")
             elTr.className = "bg-slate-100 w-full p-5 rounded-md"
             elTr.innerHTML = `
@@ -82,7 +106,7 @@ function renderUsers(list){
                            <td class="py-[7px] text-[15px] leading-[17px]">${item.email}</td>
                            <td class="py-[7px] text-[15px] leading-[17px]">${item.phone}</td>
                            <td class="py-[7px] text-[15px] leading-[17px]">${item.enrollNumber}</td>
-                           <td class="py-[7px] text-[15px] leading-[17px]">${item.dataAdmisson}</td>
+                           <td class="py-[7px] text-[15px] leading-[17px]">${item.dateAdmisson}</td>
                            <td class="py-[7px] leading-[17px]">
                            <button class="text-[25px] text-[#FEAF00] mr-3"><i class="fa-solid fa-circle-info"></i></button>
                            <button onclick="handleEdit('${item.id}')" class="text-[25px] text-[#FEAF00] mr-3"><i class="fa-solid fa-pen-to-square"></i></button>
@@ -91,7 +115,7 @@ function renderUsers(list){
             `
             list.append(elTr)
         })
-    })
+    }
 }
 renderUsers(elStudentsList)
 // Get users end
@@ -116,8 +140,8 @@ function handleEdit(id){
         elModalInner.innerHTML = `
         <form class="edit_form flex flex-col justify-center gap-5">
               <label class="block  mx-auto w-[400px]">
-                  <input type="file" class="input_file hidden">
-                  <img src="./images/imageD.png" alt="altImage" width="400" height="200" class="image w-full">
+                  <input type="file" class="input_file_edit hidden">
+                  <img src="${res.data.image}" alt="altImage" width="400" height="200" class="image_Edit w-full">
               </label>
 
               <div>
@@ -129,8 +153,6 @@ function handleEdit(id){
                 <input value="${res.data.phone}" required type="tel" name="phone" placeholder="Phone" class="w-full border h-[50px] rounded-md text-[20px] p-2 border-[2px] outline-none focus:border-yellow-500">
                 <label class="text-[20px] text-slate-500 font-bold">Enroll Number</label>
                 <input value="${res.data.enrollNumber}" required type="number" name="enrollNumber" placeholder="Enroll Number" class="w-full border h-[50px] rounded-md text-[20px] p-2 border-[2px] outline-none focus:border-yellow-500">
-                <label class="text-[20px] text-slate-500 font-bold">Date admission</label>
-                <input value="${res.data.dateAdmisson}" type="date" name="dateAdmisson" placeholder="Date admission" class="w-full border h-[50px] rounded-md text-[20px] p-2 border-[2px] outline-none focus:border-yellow-500">
               </div>
               <button type="submit" class="addBtn w-full bg-yellow-500 h-[50px] rounded-xl font-bold text-white text-[20px]">Edit</button>
            </form>
@@ -138,11 +160,11 @@ function handleEdit(id){
     })
     
 
-    let elInputFile = document.querySelector(".dateAdmisson")
-    let elImage = document.querySelector(".image")
+    let elInputEditFile = document.querySelector(".input_file_edit")
+    let elEditImage = document.querySelector(".image_Edit")
 
-    elInputFile.addEventListener("change", (e)=> {
-        elImage.src = URL.createObjectURL(e.target.files[0])
+    elInputEditFile.addEventListener("change", (e)=> {
+        elEditImage.src = URL.createObjectURL(e.target.files[0])
     })
 
     const elStudentsForm = document.querySelector(".edit_form")
@@ -155,8 +177,8 @@ function handleEdit(id){
             email:e.target.email.value,
             phone:e.target.phone.value,
             enrollNumber:e.target.enrollNumber.value,
-            dateAdmisson:e.target.dateAdmisson.value,
-            image:elImage.src
+            dateAdmisson:signedDate,
+            image:elEditImage.src
         }
 
         axios.put(HTTP, editdata).then(res => {
@@ -165,8 +187,25 @@ function handleEdit(id){
         })
     })
 }
-
 // Edit Partd end
+
+
+// Sort function start
+let isAscending = true
+function handleSortBtn(){
+    axios.get(HTTP)
+    .then(res => {
+       let sortdData = res.data.sort((a, b) => {
+          if(isAscending){
+             return a.username.localeCompare(b.username)
+          }
+       });
+        renderUsers(elStudentsList, sortdData)
+        isAscending = !isAscending
+    })
+}
+// Sort function end
+
 
 
 // Logout function start
